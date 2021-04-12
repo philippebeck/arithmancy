dadb0b4801dd3c78821fcc4b49055375902ee451
Philippe Beck <philippe@philippebeck.net>
2021-04-12 22:14:08 +0200

    Create HomeController + the 3 Project Controllers

---
+++ src/Controller/DefinitionController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class DefinitionController
+ * @package App\Controller
+ */
+class DefinitionController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render("front/definitions.twig");
+    }
+}
---
+++ src/Controller/NumberController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class NumberController
+ * @package App\Controller
+ */
+class NumberController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        return $this->render("front/numbers.twig");
+    }
+}
