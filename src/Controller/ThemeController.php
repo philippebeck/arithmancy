41583f77b4e2a6f17986a2dee1136f1ef81c36e4
Philippe Beck <philippe@philippebeck.net>
2021-04-17 21:21:26 +0200

    Create setNuumberData() for ThemeController refactoring

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -43,7 +43,7 @@ class ThemeController extends CalculationController
             $this->setPersonalData();       
             $this->setHereditaryData();       
             $this->setPowerData();       
-            $this->setSpiritualData();       
+            $this->setSpiritualData();
 
             return $this->render("front/theme.twig", [
                 "numbers" => $this->numbers
@@ -54,14 +54,33 @@ class ThemeController extends CalculationController
     }
 
     /**
-     * @param int $number
+     * @param array $numbers
      * @param string $category
      */
-    private function checkSimpleNumber(int $number, string $category)
+    private function setNumberData(
+        array $numbers,
+        string $category)
+    {
+        $this->numbers[$category . "Digit"]   = $numbers[0];
+        $this->numbers[$category . "Number"]  = $numbers[1];
+
+        $this->numbers[$category . "MainText"] = 
+        $this->allNumbers[$numbers[0] - 1][$category];
+    }
+
+    /**
+     * @param int $number
+     * @param string $category
+     * @param string $order
+     */
+    private function checkSimpleNumber(
+        int $number, 
+        string $category,
+        string $order = "Second")
     {
         if ($number < 79) {
 
-            $this->numbers[$category . "SecondText"] = 
+            $this->numbers[$category . $order . "Text"] = 
             $this->allNumbers[$number - 1]["description"];
         }  
     }
@@ -69,17 +88,44 @@ class ThemeController extends CalculationController
     /**
      * @param int $number
      * @param string $category
+     * @param string $order
      */
-    private function checkSpecialNumber(int $number, string $category)
+    private function checkDoubleNumber(
+        int $number, 
+        string $category,
+        string $order = "Second")
     {
         if ($number < 79) {
             if ($number === 11 || $number === 22) {
 
-                $this->numbers[$category . "SecondText"] = 
+                $this->numbers[$category . $order . "Text"] = 
                 $this->allNumbers[$number - 1][$category];
 
             } else {
-                $this->numbers[$category . "SecondText"] = 
+                $this->numbers[$category . $order . "Text"] = 
+                $this->allNumbers[$number - 1]["description"];
+            }
+        }
+    }
+
+    /**
+     * @param int $number
+     * @param string $category
+     * @param string $order
+     */
+    private function checkTripleNumber(
+        int $number, 
+        string $category,
+        string $order = "Second")
+    {
+        if ($number < 79) {
+            if ($number === 11 || $number === 22 || $number === 33) {
+
+                $this->numbers[$category . $order . "Text"] = 
+                $this->allNumbers[$number - 1][$category];
+
+            } else {
+                $this->numbers[$category . $order . "Text"] = 
                 $this->allNumbers[$number - 1]["description"];
             }
         }
@@ -89,16 +135,19 @@ class ThemeController extends CalculationController
      * @param int $number
      * @param string $category
      */
-    private function checkVerySpecialNumber(int $number, string $category)
+    private function checkQuadrupleNumber(
+        int $number, 
+        string $category,
+        string $order = "Second")
     {
         if ($number < 79) {
             if ($number === 11 || $number === 22 || $number === 33 || $number === 44) {
 
-                $this->numbers[$category . "SecondText"] = 
+                $this->numbers[$category . $order . "Text"] = 
                 $this->allNumbers[$number - 1][$category];
 
             } else {
-                $this->numbers[$category . "SecondText"] = 
+                $this->numbers[$category . $order . "Text"] = 
                 $this->allNumbers[$number - 1]["description"];
             }
         }
@@ -108,52 +157,35 @@ class ThemeController extends CalculationController
     {
         $lifePathNumbers = $this->getLifePathNumbers();
 
-        $this->numbers["lifePathDigit"]     = $lifePathNumbers[2];
-        $this->numbers["lifePathNumber"]    = $lifePathNumbers[0];
+        $this->numbers["lifePathSecondNumber"] = $lifePathNumbers[2];
 
-        $this->numbers["lifePathMainText"] = 
-        $this->allNumbers[$lifePathNumbers[2] - 1]["lifePath"];
-
-        $this->checkVerySpecialNumber($lifePathNumbers[0], "lifePath");
+        $this->setNumberData($lifePathNumbers, "lifePath");
+        $this->checkQuadrupleNumber($lifePathNumbers[1], "lifePath");
+        $this->checkQuadrupleNumber($lifePathNumbers[2], "lifePath", "Third");
     }
 
     private function setExpressionData()
     {
         $expressionNumbers = $this->getExpressionNumbers();
 
-        $this->numbers["expressionDigit"]   = $expressionNumbers[1];
-        $this->numbers["expressionNumber"]  = $expressionNumbers[0];
-
-        $this->numbers["expressionMainText"] = 
-        $this->allNumbers[$expressionNumbers[1] - 1]["expression"];
-
-        $this->checkSpecialNumber($expressionNumbers[0], "expression");
+        $this->setNumberData($expressionNumbers, "expression");
+        $this->checkDoubleNumber($expressionNumbers[1], "expression");
     }
 
     private function setIntimateData()
     {
         $intimateNumbers = $this->getIntimateNumbers();
 
-        $this->numbers["intimateDigit"]     = $intimateNumbers[1];
-        $this->numbers["intimateNumber"]    = $intimateNumbers[0];
-
-        $this->numbers["intimateMainText"] = 
-        $this->allNumbers[$intimateNumbers[1] - 1]["intimate"];
-
-        $this->checkSpecialNumber($intimateNumbers[0], "intimate");
+        $this->setNumberData($intimateNumbers, "intimate");
+        $this->checkDoubleNumber($intimateNumbers[1], "intimate");
     }
 
     private function setRealizationData()
     {
         $realizationNumbers = $this->getRealizationNumbers();
 
-        $this->numbers["realizationDigit"]  = $realizationNumbers[1];
-        $this->numbers["realizationNumber"] = $realizationNumbers[0];
-
-        $this->numbers["realizationMainText"] = 
-        $this->allNumbers[$realizationNumbers[1] - 1]["realization"];
-
-        $this->checkSpecialNumber($realizationNumbers[0], "realization");
+        $this->setNumberData($realizationNumbers, "realization");
+        $this->checkDoubleNumber($realizationNumbers[1], "realization");
     }
 
     private function setDayData()
@@ -170,79 +202,39 @@ class ThemeController extends CalculationController
     {
         $goalNumbers = $this->getGoalNumbers();
 
-        $this->numbers["goalDigit"]     = $goalNumbers[1];
-        $this->numbers["goalNumber"]    = $goalNumbers[0];
-
-        $this->numbers["goalMainText"] = 
-        $this->allNumbers[$goalNumbers[1] - 1]["goal"];
-
-        if ($goalNumbers[0] < 79) {
-            if (
-                $goalNumbers[0] === 11
-                ||
-                $goalNumbers[0] === 22
-                ||
-                $goalNumbers[0] === 33
-            ) {
-                $this->numbers["goalSecondText"] = 
-                $this->allNumbers[$goalNumbers[0] - 1]["goal"];
-
-            } else {
-                $this->numbers["goalSecondText"] = 
-                $this->allNumbers[$goalNumbers[0] - 1]["description"];
-            }
-        }
+        $this->setNumberData($goalNumbers, "goal");
+        $this->checkTripleNumber($goalNumbers[1], "goal");
     }
 
     private function setPersonalData()
     {
         $personalNumbers = $this->getPersonalNumbers();
 
-        $this->numbers["personalDigit"]     = $personalNumbers[1];
-        $this->numbers["personalNumber"]    = $personalNumbers[0];
-
-        $this->numbers["personalMainText"] = 
-        $this->allNumbers[$personalNumbers[1] - 1]["personal"];
-
-        $this->checkSimpleNumber($personalNumbers[0], "personal");        
+        $this->setNumberData($personalNumbers, "personal");
+        $this->checkSimpleNumber($personalNumbers[1], "personal");        
     }
 
     private function setHereditaryData()
     {
         $hereditaryNumbers = $this->getHereditaryNumbers();
 
-        $this->numbers["hereditaryDigit"]   = $hereditaryNumbers[1];
-        $this->numbers["hereditaryNumber"]  = $hereditaryNumbers[0];
-
-        $this->numbers["hereditaryMainText"] = 
-        $this->allNumbers[$hereditaryNumbers[1] - 1]["hereditary"];
-
-        $this->checkSimpleNumber($hereditaryNumbers[0], "hereditary");
+        $this->setNumberData($hereditaryNumbers, "hereditary");
+        $this->checkSimpleNumber($hereditaryNumbers[1], "hereditary");
     }
 
     private function setPowerData()
     {
         $powerNumbers = $this->getPowerNumbers();
 
-        $this->numbers["powerDigit"]    = $powerNumbers[1];
-        $this->numbers["powerNumber"]   = $powerNumbers[0];
-
-        $this->numbers["powerMainText"] = 
-        $this->allNumbers[$powerNumbers[1] - 1]["power"];
-
-        $this->checkSpecialNumber($powerNumbers[0], "power");
+        $this->setNumberData($powerNumbers, "power");
+        $this->checkDoubleNumber($powerNumbers[1], "power");
     }
 
     private function setSpiritualData()
     {
         $spiritualNumbers = $this->getSpiritualNumbers();
 
-        $this->numbers["spiritualDigit"]    = $spiritualNumbers[1];
-        $this->numbers["spiritualNumber"]   = $spiritualNumbers[0];
-
-        $this->numbers["spiritualMainText"] = 
-        $this->allNumbers[$spiritualNumbers[1] - 1]["spiritual"];
-
-        $this->checkVerySpecialNumber($spiritualNumbers[0], "spiritual");
+        $this->setNumberData($spiritualNumbers, "spiritual");
+        $this->checkQuadrupleNumber($spiritualNumbers[1], "spiritual");
     }
 }
