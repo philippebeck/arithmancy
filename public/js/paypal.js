014f1ac15b77d10416bcfd9e6ec3cd87831b1380
Philippe Beck <philippe@philippebeck.net>
2021-04-21 20:43:06 +0200

    Create paypal.js

---
+++ public/js/paypal.js
@@ -0,0 +1,40 @@
+paypal.Buttons({
+  style: {
+    color:  "blue",
+    shape:  "pill",
+    label:  "pay"
+  },
+
+  createOrder : function (data, actions) {
+
+    return actions.order.create({
+
+      purchase_units : [{
+        amount : {
+          value : "10.00",
+          currency_code : "EUR"
+        }
+      }]
+    });
+  },
+
+  onApprove : function (data, actions) {
+
+    return actions.order.capture().then(
+
+      function(details) {
+        console.log(details);
+        
+        alert("Transaction validée par " + 
+        details.payer.name.given_name + 
+        details.payer.name.surname);
+      }
+    );
+  },
+
+  onCancel : function (data) {
+    alert("Transaction annulée !");
+  }
+
+})
+.render("#paypal-button");
