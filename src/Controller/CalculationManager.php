29a0a683281bca0e55ebd3e575399945f3a42f39
Philippe Beck <philippe@philippebeck.net>
2021-04-30 13:21:13 +0200

    Create personal year, month & day calculations

--- src/Controller/CalculationManager.php
+++ src/Controller/CalculationManager.php
@@ -390,4 +390,50 @@ abstract class CalculationManager extends MainController
 
         return [$spiritualDigit, $spiritualNumber];
     }
+
+     // ***************************************************************** \\
+    // ******************** PREDICTION NUMBERS GETTERS ******************** \\
+
+    /**
+     * @param int $year
+     * @return array
+     */
+    protected function getPersonalYearNumbers(int $year)
+    {
+        $personalYearNumber = 
+            $this->birthDate["day"] + 
+            $this->birthDate["month"] + 
+            $year;
+
+        $personalYearDigit = $this->getDigitFromNumber($personalYearNumber);
+
+        return [$personalYearDigit, $personalYearNumber];
+    }
+
+    /**
+     * @param int $month
+     * @param int $year
+     * @return array
+     */
+    protected function getPersonalMonthNumbers(int $month, int $year)
+    {
+        $personalMonthNumber    = $this->getPersonalYearNumbers($year) + $month;
+        $personalMonthDigit     = $this->getDigitFromNumber($personalMonthNumber);
+
+        return [$personalMonthDigit, $personalMonthNumber];
+    }
+
+    /**
+     * @param int $day
+     * @param int $month
+     * @param int $year
+     * @return array
+     */
+    protected function getPersonalDayNumbers(int $day, int $month, int $year)
+    {
+        $personalDayNumber  = $this->getPersonalMonthNumbers($month, $year) + $day;
+        $personalDayDigit   = $this->getDigitFromNumber($personalDayNumber);
+
+        return [$personalDayDigit, $personalDayNumber];
+    }
 }
